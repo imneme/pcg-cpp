0a79c85e5f9e16abff813f9e0df14fc4a4ccc4ba
Melissa O'Neill <oneill@acm.org>
2017-04-28 00:58:46 -0700

    Generalize to include dual MIT/Apache-2.0 license, as promised long ago.

--- sample/cppref-sample.cpp
+++ sample/cppref-sample.cpp
@@ -2,7 +2,10 @@
  * Random Number Generation Example
  *
  * Copyright 2012-2014 cppreference.com Contributors
- * Copyright 2014 Melissa O'Neill <oneill@pcg-random.org>
+ * Copyright 2014-2017 Melissa O'Neill <oneill@pcg-random.org>
+ *                     and the PCG Project contributors.
+ *
+ * SPDX-License-Identifier: (CC-BY-SA-3.0 OR Apache-2.0 OR MIT)
  *
  * Code in this file is based on code from cppreference.com, specifically 
  * the sample code at http://en.cppreference.com/w/cpp/numeric/random
@@ -10,22 +13,17 @@
  * license.  You may distribute this file (and only this file) under
  * that license, see http://creativecommons.org/licenses/by-sa/3.0/.
  *
- * Also licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with a valid License.
- * You may obtain a copy of the Apache License at
+ * Also licensed under the Apache License, Version 2.0 (provided in
+ * LICENSE-APACHE.txt and at http://www.apache.org/licenses/LICENSE-2.0)
+ * or under the MIT license (provided in LICENSE-MIT.txt and at
+ * http://opensource.org/licenses/MIT), at your option. This file may not
+ * be copied, modified, or distributed except according to those terms.
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Distributed on an "AS IS" BASIS, WITHOUT WARRANTY OF ANY KIND, either
+ * express or implied.  See your chosen license for details.
  *
  * For additional information about the PCG random number generation scheme,
- * including its license and other licensing options, visit
- *
- *     http://www.pcg-random.org
+ * visit http://www.pcg-random.org/.
  */
 
 /*
